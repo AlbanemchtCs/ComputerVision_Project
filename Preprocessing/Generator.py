412b4f7ef25348f738f963f3a4e5e094dde1e722
Rodolphe Nonclercq <rodolphe.nonclercq@student-cs.fr>
2023-04-12 12:36:06 +0200

    invertColor -> permuteColor

--- Preprocessing/Generator.py
+++ Preprocessing/Generator.py
@@ -11,23 +11,37 @@ import matplotlib.pyplot as plt
 def generator_data(batch_input,batch_output, p =None):
     if p is None:
         p = {'RHF' : 0.5,
-             'RVF' : 0.5,
-             'RI' : 0.5}
+             'RVF' : 0.5}
     # Normalization
     batch_input,batch_output = batch_input/255,batch_output/255
     # def transformations
     transformations_h = transforms.RandomHorizontalFlip(p=1)
     transformations_v = transforms.RandomVerticalFlip(p=1)
-    invert_color = transforms.RandomInvert(p=1)
     if random.uniform(0, 1) > p['RHF']:
         batch_input = transformations_h(batch_input)
         batch_output = transformations_h(batch_output)
     if random.uniform(0, 1) > p['RVF']:
         batch_input = transformations_v(batch_input)
         batch_output = transformations_v(batch_output) 
-    if random.uniform(0, 1) > p['RI']:
-        batch_input = invert_color(batch_input)
-        batch_output = invert_color(batch_output)
+    cop_input = batch_input
+    cop_output = batch_output
+    for i in range(len(batch_input)):
+        r = random.randint(0,3)
+        if r==1:
+            batch_input[i,0] = cop_input[i,1]
+            batch_input[i,1] = cop_input[i,0]
+            batch_output[i,0] = cop_output[i,1]
+            batch_output[i,1] = cop_output[i,0]
+        elif r==2:
+            batch_input[i,1] = cop_input[i,2]
+            batch_input[i,2] = cop_input[i,1]
+            batch_output[i,1] = cop_output[i,2]
+            batch_output[i,2] = cop_output[i,1]
+        elif r==3:
+            batch_input[i,0] = cop_input[i,2]
+            batch_input[i,2] = cop_input[i,0]
+            batch_output[i,0] = cop_output[i,2]
+            batch_output[i,2] = cop_output[i,0]
     
     return batch_input,batch_output
 
